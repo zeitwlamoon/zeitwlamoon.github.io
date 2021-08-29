cc81e8f68347a7c1a81c96a957add0de22804d48
Brikaa <brikaaomar@gmail.com>
2021-08-29 15:42:08 +0200

    Add contact section

---
+++ js/scroll.js
@@ -0,0 +1,13 @@
+(function() {
+    function add_events() {
+        document.getElementById('menu-button').addEventListener('click', () => scroll_to('menu-content'));
+        document.getElementById('menu-dropdown').addEventListener('change', () => scroll_to('menu-content'));
+        document.getElementById('contact-button').addEventListener('click', () => scroll_to('contact-us'))
+    }
+
+    function scroll_to(element_id) {
+        document.getElementById(element_id).scrollIntoView();
+    }
+
+    add_events();
+})();
